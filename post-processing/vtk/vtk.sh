d7a9ef46e8224cc83949f7067e34008e96ec37f7
Titanium Alloys Design Engine <shi.103@osu.edu>
2017-03-27 10:20:10 -0400

    Update vtk.sh

--- post-processing/vtk/vtk.sh
+++ post-processing/vtk/vtk.sh
@@ -1,1 +1,16 @@
 
+for i in {1..10}
+do
+FILES1=field_${i}.dat
+FILES1a=field_${i}.vtk
+
+for f in $FILES1
+do 
+    echo "Processing $f file..."
+    cat vtkhead $FILES1 > $FILES1a
+
+    
+done
+
+done
+echo "Job Done!"
